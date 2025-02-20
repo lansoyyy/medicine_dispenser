76ba1efd5e6973ac92b5febbd78421871da19685
Lans12345 <olanalans12345@gmail.com>
2025-02-21 07:02:01 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:medicine_dispender/firebase_options.dart';
 import 'package:medicine_dispender/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'medicine-dispenser-4f53b',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
